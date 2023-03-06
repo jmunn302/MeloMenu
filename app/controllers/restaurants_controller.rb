b9351920b522e937cb8f224fa5f7123f48248e6e
Emilia Jung <emilia.jung04@gmail.com>
2023-03-06 15:35:17 +0100

    restaurant controller and index/show view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,51 @@
 class RestaurantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = current_user.restaurants
+  end
+
+  def show
+    @menus = @restaurant.menus
+  end
+
+  def new
+    @restaurant = current_user.restaurants.build
+  end
+
+  def create
+    @restaurant = current_user.restaurants.build(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully deleted.'
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = current_user.restaurants.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :type, :address, :phone_number, :website, :email)
+  end
 end
