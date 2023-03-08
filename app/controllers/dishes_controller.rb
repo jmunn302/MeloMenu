a7e85393d276ea8eaa3888ca69d0258365741ca9
Julie Munn <jmunn302@gmail.com>
2023-03-08 09:05:31 +0100

    fix dishes params

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -21,12 +21,12 @@ class DishesController < ApplicationController
 
   def create
     @dish = Dish.new(dish_params)
-    @menu = Menu.find(params[:menu_id])
+    @dish.menu = Menu.find(params[:menu_id])
     if @dish.save
       redirect_to @dish.menu, notice: "Menu was successfully created."
     else
-      render :new, status: :unprocessable_entity
       raise
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -46,8 +46,10 @@ class DishesController < ApplicationController
       @dish = Dish.find(params[:id])
     end
 
+
+
   def dish_params
-    params.require(:dish).permit(:name, :description, :price, :category)
+    params.require(:dish).permit(:name, :description, :price, :category, dietary_type: [])
   end
 
 end
