965d6f3a88b83d43487bdcfd1d8251d699a9039a
Julie Munn <jmunn302@gmail.com>
2023-03-10 11:59:21 +0100

    heorku trial

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dobohmdlf'
+  config.api_key = '361289386713888'
+  config.api_secret = '0nmKBH831ftju0xFvok-BG_RF2Q'
+  config.secure = true
+end
