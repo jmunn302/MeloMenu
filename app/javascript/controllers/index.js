0ce11c5acaec657389b416da26a7a27952a32fac
Julie Munn <jmunn302@gmail.com>
2023-03-10 10:18:30 +0100

    make it better

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,12 +2,11 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import { application } from "controllers/application"
+import { app_application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
