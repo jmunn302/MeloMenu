9587c088b37de549eece7595182db32bdf803bc0
Julie Munn <jmunn302@gmail.com>
2023-03-10 15:54:10 +0100

    css and flow changes

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'dobohmdlf'
-  config.api_key = '361289386713888'
-  config.api_secret = '0nmKBH831ftju0xFvok-BG_RF2Q'
-  config.secure = true
-end
